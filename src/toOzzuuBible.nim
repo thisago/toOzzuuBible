956454d277838b24c69cbfa80ef7b74b2f6f876a
Thiago Navarro <thiago@oxyoy.com>
2023-04-07 15:33:49 +0000

    Fixed verse searching

--- src/toOzzuuBible.nim
+++ src/toOzzuuBible.nim
@@ -1,41 +1,40 @@
 import std/dom except find
-from std/jsre import match, newRegExp, exec
-import std/jsconsole
 from std/strutils import strip, replace
 from std/base64 import decode
+from std/strformat import fmt
 
 from pkg/util/forStr import getAllEnclosedText
-from pkg/bibleTools import parseBibleVerse, inOzzuuBible, `$`
+from pkg/bibleTools import parseBibleVerses, inOzzuuBible, `$`
 
 proc find(w: Window, text: cstring, caseSensitive = false,
            backwards = false): bool {.importCpp.}
   ## Waiting merging of https://github.com/nim-lang/Nim/pull/21621
 proc deleteContents(r: Range) {.importCpp.}
-proc toString[T](x: T): cstring {.importCpp.}
-
-let verseRegex = newRegExp(`$`(bibleTools.verseRegex.toString)[1..^2], "g")
+proc empty(r: Selection) {.importCpp.}
 
 proc main =
   ## Main function
   for enclosed in getAllEnclosedText($document.body.innerText):
     for text in enclosed.data.texts:
-      let text = text.replace(".", "")
-      for singleVerse in text.strip.match verseRegex:
-        let verse = parseBibleVerse $singleVerse
-        if verse.error:
+      for verse in text.parseBibleVerses:
+        if verse.parsed.error:
           continue
-        while window.find cstring text:
-          let rng = document.getSelection.getRangeAt 0
+        echo verse.raw
+        while window.find cstring verse.raw:
+          let
+            selection = document.getSelection
+            rng = selection.getRangeAt 0
+          empty selection
           rng.deleteContents
           var link = document.createElement "a"
-          link.innerText = `$`(verse, hebrewTransliteration = true, shortBook = false)
-          link.setAttribute("href", verse.inOzzuuBible)
+          link.innerText = `$`(verse.parsed, hebrewTransliteration = true, shortBook = false)
+          link.setAttribute("href", verse.parsed.inOzzuuBible)
           link.setAttribute("target", "_blank")
           link.setAttribute("rel", "noopener noreferrer")
+          link.setAttribute("title", verse.raw)
           link.setAttribute("class", "toOzzuuBible")
           rng.insertNode link
-          echo text
-
+          echo "matched: ", verse.raw
 
 document.addEventListener("DOMContentLoaded", proc (ev: Event) =
   # Styling
@@ -44,5 +43,5 @@ document.addEventListener("DOMContentLoaded", proc (ev: Event) =
   style.innerHTML = css.`$`.decode.cstring
   document.head.appendChild style
   
-  main()
+  discard setTimeout(main, 500)
 )
